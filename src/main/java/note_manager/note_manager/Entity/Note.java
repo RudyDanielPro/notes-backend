62f62461ac2b9d5ee67d5599fea2fb2bc340fca9
Rudy <rudydanielcarballo@gmail.com>
2026-03-02 12:36:49 -0500

    Cambiando limites de caracteres

--- src/main/java/note_manager/note_manager/Entity/Note.java
+++ src/main/java/note_manager/note_manager/Entity/Note.java
@@ -30,7 +30,7 @@ public class Note {
     @Column(name = "Título", nullable = false)
     private String titulo;
 
-    @Column(name = "Contenido", nullable = false)
+    @Column(name = "Contenido", nullable = false, length = 100000)
     private String contenido;
 
     @Column(name = "Fecha", nullable = false)
